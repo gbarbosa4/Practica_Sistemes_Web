5448baf059bbe7bc4bf022e7b4685f2ad3966f8b
Guillem <guillem4_uel@hotmail.com>
2014-03-25 13:01:20 +0100

    Fourth commit

--- Concessionari/iConcessionari/models.py
+++ Concessionari/iConcessionari/models.py
@@ -1,21 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class Client(models.Model):
-	name = models.TextField(max_length=100)
+class Factory(models.Model):
+	Factory_id = models.TextField(max_length=100)
 	city = models.TextField(max_length=100)
 	address = models.TextField(max_length=100)
 	telephone = models.IntegerField()
-	dni = models.TextField(max_length=9)
-	
-class Car(models.Model):
-	brand = models.TextField(max_length=30)
-	model = models.TextField(max_length=30)
-	price = models.IntegerField()
-	car_id = models.IntegerField()
+	production = models.IntegerField()
+	stock = models.IntegerField()
 
-class Comercial(models.Model):
+class Comercial(User):
 	company_id = models.TextField(max_length=100)
 	name = models.TextField(max_length=100)
 	city = models.TextField(max_length=100)
@@ -23,15 +18,26 @@ class Comercial(models.Model):
 	telephone = models.IntegerField()
 	dni = models.TextField(max_length=9)
 	sells = models.IntegerField()
+	factory = models.ManyToMany(Factory)
 
-class Factory(models.Model):
-	Factory_id = models.TextField(max_length=100)
+class Client(User):
+	name = models.TextField(max_length=100)
 	city = models.TextField(max_length=100)
 	address = models.TextField(max_length=100)
 	telephone = models.IntegerField()
-	brand_car = models.TextField(max_length=30)
-	production = models.IntegerField()
-	stock = models.IntegerField()
+	dni = models.TextField(max_length=9)
+	comercial = models.ForeignKey(Comercial)
+
+class Car(models.Model):
+	brand = models.TextField(max_length=30)
+	model = models.TextField(max_length=30)
+	price = models.IntegerField()
+	car_id = models.IntegerField()
+	client = models.ForeignKey(Client)
+	comercial = models.ForeignKey(Comercial)
+	factory = models.ForeignKey(Factory)
+
+
 	
 
 
